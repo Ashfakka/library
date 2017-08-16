3a7977ac805961c0bde66dab94066f56b7b0cba1
RAPIDVALUE\ashfak.ka <ashfaknav001@gmail.com>
2017-08-16 19:09:36 +0530

    no message

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -36,14 +36,14 @@ export class UserComponent implements OnInit {
 
    openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    // document.getElementById("main").style.marginLeft = "250px";
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
 
   closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
+    // document.getElementById("main").style.marginLeft = "0";
+    // document.body.style.backgroundColor = "white";
   }
 
 }
